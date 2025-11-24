e56fca3a7886f98408b6a8c95ba2383bc615477e
shaozheng <shaozheng.wu@gmail.com>
2025-11-24 18:22:35 +0800

    fix channel not display issue

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeKidsSearchExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeKidsSearchExtractor.java
@@ -32,128 +32,151 @@ import javax.annotation.Nonnull;
 
 public class YoutubeKidsSearchExtractor extends SearchExtractor {
 
-    private JsonObject initialData;
+        private final boolean extractVideoResults;
+        private final boolean extractChannelResults;
+        private JsonObject initialData;
 
-    public YoutubeKidsSearchExtractor(final StreamingService service,
-            final SearchQueryHandler linkHandler) {
-        super(service, linkHandler);
-    }
+        public YoutubeKidsSearchExtractor(final StreamingService service,
+                        final SearchQueryHandler linkHandler) {
+                super(service, linkHandler);
+                // YouTube Kids doesn't support search filters (videos-only, channels-only,
+                // etc.)
+                // So we always extract both video and channel results
+                extractVideoResults = true;
+                extractChannelResults = true;
+        }
 
-    @Override
-    public void onFetchPage(@Nonnull final Downloader downloader)
-            throws IOException, ExtractionException {
+        @Override
+        public void onFetchPage(@Nonnull final Downloader downloader)
+                        throws IOException, ExtractionException {
 
-        final String query = super.getSearchString();
+                final String query = super.getSearchString();
 
-        // Build request JSON body based on the provided curl command
-        final JsonBuilder<JsonObject> bodyBuilder = prepareDesktopJsonBuilder(
-                getExtractorLocalization(), getExtractorContentCountry())
-                .value("context", prepareDesktopJsonBuilder(
-                        getExtractorLocalization(), getExtractorContentCountry())
-                        .value("client", prepareDesktopJsonBuilder(
+                // Build request JSON body based on the provided curl command
+                final JsonBuilder<JsonObject> bodyBuilder = prepareDesktopJsonBuilder(
                                 getExtractorLocalization(), getExtractorContentCountry())
-                                .value("clientName", "WEB_KIDS")
-                                .value("clientVersion", "2.20251120.00.00")
-                                .value("kidsAppInfo", prepareDesktopJsonBuilder(
-                                        getExtractorLocalization(), getExtractorContentCountry())
-                                        .value("contentSettings", prepareDesktopJsonBuilder(
+                                .value("context", prepareDesktopJsonBuilder(
                                                 getExtractorLocalization(), getExtractorContentCountry())
-                                                .value("corpusPreference", "KIDS_CORPUS_PREFERENCE_TWEEN")
-                                                .value("kidsNoSearchMode", "YT_KIDS_NO_SEARCH_MODE_OFF")
+                                                .value("client", prepareDesktopJsonBuilder(
+                                                                getExtractorLocalization(),
+                                                                getExtractorContentCountry())
+                                                                .value("clientName", "WEB_KIDS")
+                                                                .value("clientVersion", "2.20251120.00.00")
+                                                                .value("kidsAppInfo", prepareDesktopJsonBuilder(
+                                                                                getExtractorLocalization(),
+                                                                                getExtractorContentCountry())
+                                                                                .value("contentSettings",
+                                                                                                prepareDesktopJsonBuilder(
+                                                                                                                getExtractorLocalization(),
+                                                                                                                getExtractorContentCountry())
+                                                                                                                .value("corpusPreference",
+                                                                                                                                "KIDS_CORPUS_PREFERENCE_TWEEN")
+                                                                                                                .value("kidsNoSearchMode",
+                                                                                                                                "YT_KIDS_NO_SEARCH_MODE_OFF")
+                                                                                                                .done())
+                                                                                .done())
+                                                                .done())
                                                 .done())
-                                        .done())
-                                .done())
-                        .done())
-                .value("query", query);
+                                .value("query", query);
 
-        final byte[] postBody = JsonWriter.string(bodyBuilder.done())
-                .getBytes(StandardCharsets.UTF_8);
+                final byte[] postBody = JsonWriter.string(bodyBuilder.done())
+                                .getBytes(StandardCharsets.UTF_8);
 
-        initialData = getJsonPostResponse("search", postBody, getExtractorLocalization());
-    }
-
-    @Nonnull
-    @Override
-    public String getName() {
-        return "YouTube Kids";
-    }
-
-    @Nonnull
-    @Override
-    public String getSearchSuggestion() throws ParsingException {
-        final JsonObject itemSectionRenderer = initialData.getObject("contents")
-                .getObject("sectionListRenderer")
-                .getArray("contents")
-                .getObject(0)
-                .getObject("itemSectionRenderer");
-        final JsonObject didYouMeanRenderer = itemSectionRenderer.getArray("contents")
-                .getObject(0)
-                .getObject("didYouMeanRenderer");
-
-        if (!didYouMeanRenderer.isEmpty()) {
-            return JsonUtils.getString(didYouMeanRenderer,
-                    "correctedQueryEndpoint.searchEndpoint.query");
+                initialData = getJsonPostResponse("search", postBody, getExtractorLocalization());
         }
 
-        return Objects.requireNonNullElse(
-                getTextFromObject(itemSectionRenderer.getArray("contents")
-                        .getObject(0)
-                        .getObject("showingResultsForRenderer")
-                        .getObject("correctedQuery")),
-                "");
-    }
-
-    @Override
-    public boolean isCorrectedSearch() throws ParsingException {
-        final JsonObject showingResultsForRenderer = initialData.getObject("contents")
-                .getObject("sectionListRenderer").getArray("contents").getObject(0)
-                .getObject("itemSectionRenderer").getArray("contents").getObject(0)
-                .getObject("showingResultsForRenderer");
-        return !showingResultsForRenderer.isEmpty();
-    }
-
-    @Nonnull
-    @Override
-    public List<MetaInfo> getMetaInfo() throws ParsingException {
-        return YoutubeMetaInfoHelper.getMetaInfo(
-                initialData.getObject("contents")
-                        .getObject("sectionListRenderer")
-                        .getArray("contents"));
-    }
-
-    @Nonnull
-    @Override
-    public InfoItemsPage<InfoItem> getInitialPage() throws IOException, ExtractionException {
-        final MultiInfoItemsCollector collector = new MultiInfoItemsCollector(getServiceId());
-        final TimeAgoParser timeAgoParser = getTimeAgoParser();
-
-        // Parse response based on the provided JSON structure
-        // contents -> sectionListRenderer -> contents -> [0] -> itemSectionRenderer ->
-        // contents
-        if (initialData != null && initialData.has("contents")) {
-            JsonObject contents = initialData.getObject("contents");
-            if (contents.has("sectionListRenderer")) {
-                contents.getObject("sectionListRenderer")
-                        .getArray("contents").stream()
-                        .filter(JsonObject.class::isInstance)
-                        .map(JsonObject.class::cast)
-                        .filter(obj -> obj.has("itemSectionRenderer"))
-                        .map(obj -> obj.getObject("itemSectionRenderer"))
-                        .flatMap(section -> section.getArray("contents").stream())
-                        .filter(JsonObject.class::isInstance)
-                        .map(JsonObject.class::cast)
-                        .filter(content -> content.has("compactVideoRenderer"))
-                        .map(content -> content.getObject("compactVideoRenderer"))
-                        .forEachOrdered(videoRenderer -> collector.commit(
-                                new YoutubeStreamInfoItemExtractor(videoRenderer, timeAgoParser)));
-            }
+        @Nonnull
+        @Override
+        public String getName() {
+                return "YouTube Kids";
         }
 
-        return new InfoItemsPage<>(collector, null);
-    }
+        @Nonnull
+        @Override
+        public String getSearchSuggestion() throws ParsingException {
+                final JsonObject itemSectionRenderer = initialData.getObject("contents")
+                                .getObject("sectionListRenderer")
+                                .getArray("contents")
+                                .getObject(0)
+                                .getObject("itemSectionRenderer");
+                final JsonObject didYouMeanRenderer = itemSectionRenderer.getArray("contents")
+                                .getObject(0)
+                                .getObject("didYouMeanRenderer");
 
-    @Override
-    public InfoItemsPage<InfoItem> getPage(final Page page) throws IOException, ExtractionException {
-        return InfoItemsPage.emptyPage();
-    }
+                if (!didYouMeanRenderer.isEmpty()) {
+                        return JsonUtils.getString(didYouMeanRenderer,
+                                        "correctedQueryEndpoint.searchEndpoint.query");
+                }
+
+                return Objects.requireNonNullElse(
+                                getTextFromObject(itemSectionRenderer.getArray("contents")
+                                                .getObject(0)
+                                                .getObject("showingResultsForRenderer")
+                                                .getObject("correctedQuery")),
+                                "");
+        }
+
+        @Override
+        public boolean isCorrectedSearch() throws ParsingException {
+                final JsonObject showingResultsForRenderer = initialData.getObject("contents")
+                                .getObject("sectionListRenderer").getArray("contents").getObject(0)
+                                .getObject("itemSectionRenderer").getArray("contents").getObject(0)
+                                .getObject("showingResultsForRenderer");
+                return !showingResultsForRenderer.isEmpty();
+        }
+
+        @Nonnull
+        @Override
+        public List<MetaInfo> getMetaInfo() throws ParsingException {
+                return YoutubeMetaInfoHelper.getMetaInfo(
+                                initialData.getObject("contents")
+                                                .getObject("sectionListRenderer")
+                                                .getArray("contents"));
+        }
+
+        @Nonnull
+        @Override
+        public InfoItemsPage<InfoItem> getInitialPage() throws IOException, ExtractionException {
+                final MultiInfoItemsCollector collector = new MultiInfoItemsCollector(getServiceId());
+                final TimeAgoParser timeAgoParser = getTimeAgoParser();
+
+                // Parse response based on the provided JSON structure
+                // contents -> sectionListRenderer -> contents -> [0] -> itemSectionRenderer ->
+                // contents
+                if (initialData != null && initialData.has("contents")) {
+                        JsonObject contents = initialData.getObject("contents");
+                        if (contents.has("sectionListRenderer")) {
+                                contents.getObject("sectionListRenderer")
+                                                .getArray("contents").stream()
+                                                .filter(JsonObject.class::isInstance)
+                                                .map(JsonObject.class::cast)
+                                                .filter(obj -> obj.has("itemSectionRenderer"))
+                                                .map(obj -> obj.getObject("itemSectionRenderer"))
+                                                .flatMap(section -> section.getArray("contents").stream())
+                                                .filter(JsonObject.class::isInstance)
+                                                .map(JsonObject.class::cast)
+                                                .forEachOrdered(content -> {
+                                                        if (content.has("compactVideoRenderer")
+                                                                        && extractVideoResults) {
+                                                                collector.commit(new YoutubeStreamInfoItemExtractor(
+                                                                                content.getObject(
+                                                                                                "compactVideoRenderer"),
+                                                                                timeAgoParser));
+                                                        } else if (content.has("compactChannelRenderer")
+                                                                        && extractChannelResults) {
+                                                                collector.commit(new YoutubeChannelInfoItemExtractor(
+                                                                                content.getObject(
+                                                                                                "compactChannelRenderer")));
+                                                        }
+                                                });
+                        }
+                }
+
+                return new InfoItemsPage<>(collector, null);
+        }
+
+        @Override
+        public InfoItemsPage<InfoItem> getPage(final Page page) throws IOException, ExtractionException {
+                return InfoItemsPage.emptyPage();
+        }
 }
